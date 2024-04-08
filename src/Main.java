5a0976f7f07fa191a05d875e9b785d3cbd277f17
dimakorol777 <dimakorol77777@gmail.com>
2024-04-08 22:16:30 +0200

    sdd

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //с новой строки каждое слово
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("Dima Korol");
+        // в одну строку все слова
+        System.out.print("Hello, my name is Dima Korol" +'\n');
+
+        // или так еще можно
+        System.out.print("Hello,");
+        System.out.print(" my");
+        System.out.print(" name");
+        System.out.print(" is");
+        System.out.print(" Dima Korol");
+    }
+}
